c5bc604c3055569a1716f522697525c647ee38b8
Deepak <deepak.kadarivelu@conpend.com>
2025-01-23 15:03:39 +0530

    Simple ollama setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world");
+import { ChatOllama } from "@langchain/ollama";
+
+const llm = new ChatOllama({
+  model: "mario",
+  temperature: 1,
+  maxRetries: 2,
+  format: "json",
+});
+
+const aiMsg = await llm.invoke([
+  ["system", "Translate English to Dutch."],
+  ["human", "I love programming."],
+]);
+console.log(aiMsg);
